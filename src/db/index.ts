c8a36b24f45df336d221853c666ae75904027b3e
Ru Chern Chong <hello@ruchern.dev>
2025-11-08 00:25:33 +0800

    chore: add schema to db client for type safety

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import "dotenv/config";
+import * as schema from "./schema";
 
 export const db = drizzle({
   connection: {
     url: process.env.TURSO_DATABASE_URL as string,
     authToken: process.env.TURSO_AUTH_TOKEN as string,
   },
+  schema,
   casing: "snake_case",
 });
